f683615335c5af11a03e84ad81aafa85fd7de354
elissonguerra <elisson.guerra@gmail.com>
2024-07-30 21:55:29 -0300

    Criado o projeto MVC e baixado as dependências Entity Framework e Pomelo

---
+++ SiteInstitucionalBandup/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SiteInstitucionalBandup.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
